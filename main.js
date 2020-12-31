536a4b6dc347e7226e65af80476ddecda578e976
Sanasul786 <71544603+Sanasul786@users.noreply.github.com>
2020-12-31 14:27:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hi , This is my 1st project on console");
+
+var firstname = "Sana";
+var lastname = "Sultana";
+var full_name=firstname.concat( lastname);
+console.log(full_name);
+
+var number1 = "50";
+console.log("number1=50");
+var number2 = "5";
+console.log("number2=5");
+var add = number1+number2
+console.log(55);
+var sub = number1-number2;
+console.log(sub);
+var multi = number1*number2;
+console.log(multi);
+var divide = number1/number2;
+console.log(divide);   
